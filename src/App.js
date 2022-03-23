1224c6c89219d6ef66768d9e454f7b4203eb5861
BrendenSeifried <brendenseifried@gmail.com>
2022-03-23 11:31:51 -0700

    added booklist to apps and added useParams in bookdetail

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import BookList from './views/Books/BookList';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import BookDetail from './views/Books/BookDetail';
 import './App.css';
 
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <BrowserRouter>
+      <main className="container">
+        <h1>Library Catalog</h1>
+        <Switch>
+          <Route exact path = '/booklist'>
+            <BookList />
+          </Route>
+          {/* <BookDetail /> */}
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
--- src/views/Books/BookDetail.js
+++ src/views/Books/BookDetail.js
@@ -5,14 +5,17 @@ import { getBookById } from '../../services/books';
 
 function BookDetail() {
   // const id = 1; // TODO: Use id from route
-  const id = useParams();
+  const { id } = useParams();
   console.log(id);
   const [book, setBook] = useState(null);
+
+
 
   useEffect(() => {
     getBookById(id).then(({ data }) => setBook(data));
   }, [id]);
 
+  
   if (!book) return <h3>Loading book...</h3>;
 
   return <Book book={book} showDetail />;
